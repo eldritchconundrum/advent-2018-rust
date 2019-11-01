c79bfb151a0e7e424078e295b3aeec3c618cec7d
Eldritch Conundrum <eldritch.conundrum@gmail.com>
2019-11-01 18:42:04 +0100

    day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
+#![allow(unused_imports)] // TODO: find how to enable this warning only when I'm about to commit, not while I'm debugging
+
+macro_rules! unwrap {
+    ( $x:expr ) => { $x.expect(&format!("line {} in {}", line!(), file!())) }
+}
+
 fn main() {
-    println!("Hello, world!");
-    assert_eq!(2 + 3, 4);
+    assert_eq!(day1p1(), 516);
 }
 
-#[cfg(test)]
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
-#[test]
-fn test1() {
-    assert_eq!(2 + 3, 4);
+fn day1p1() -> i64 {
+    println!("{}", unwrap!(unwrap!(std::env::current_dir()).to_str()));
+    let filename = "inputs-2018/input1";
+    let file = unwrap!(File::open(filename));
+    let reader = BufReader::new(file);
+    let mut sum = 0;
+    for line in reader.lines().map(|s| unwrap!(s)) {
+        //println!("{}", line);
+        let n = unwrap!(line.parse::<i64>());
+        sum += n;
+    }
+    sum
 }
+
+
+
+// TODO: setup rust-fmt with vscode
+
+
+// #[cfg(test)]
+// #[test]
+// fn test1() {
+//     assert_eq!(2 + 2, 4);
+// }
