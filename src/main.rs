bd17276d8f2d553306837d8a59458b23727bac87
Eldritch Conundrum <eldritch.conundrum@gmail.com>
2019-11-02 14:31:06 +0100

    day1p2 + upgrade error macro

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,57 @@
 #![allow(unused_imports)] // TODO: find how to enable this warning only when I'm about to commit, not while I'm debugging
 
-macro_rules! unwrap {
-    ( $x:expr ) => { $x.expect(&format!("line {} in {}", line!(), file!())) }
+macro_rules! e {
+    ($fmt:expr, $($e:expr),*) => { &format!(concat!("{}:{}: ", $fmt), file!(), line!(), $($e),*) };
+    ($e:expr) => { &format!("{}:{}: {}", file!(), line!(), $e) };
+    () => { &format!("{}:{}", file!(), line!()) };
 }
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+fn strings_input(i: i64) -> Vec<String> {
+    use std::fs::File;
+    use std::io::{prelude::*, BufReader};
+    let filename = &format!("inputs-2018/input{}", i);
+    let file = File::open(filename).expect(e!(filename));
+    let reader = BufReader::new(file);
+    let v = reader.lines().map(|s| s.expect(e!())).collect();
+    v
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
 
 fn main() {
+    // println!("{}", std::env::current_dir().expect(e!()).to_str().expect(e!()));
     assert_eq!(day1p1(), 516);
+    assert_eq!(day1p2(), 71892);
 }
-
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
 
 fn day1p1() -> i64 {
-    println!("{}", unwrap!(unwrap!(std::env::current_dir()).to_str()));
-    let filename = "inputs-2018/input1";
-    let file = unwrap!(File::open(filename));
-    let reader = BufReader::new(file);
-    let mut sum = 0;
-    for line in reader.lines().map(|s| unwrap!(s)) {
-        //println!("{}", line);
-        let n = unwrap!(line.parse::<i64>());
-        sum += n;
-    }
-    sum
+    let numbers: Vec<i64> = strings_input(1).into_iter().map(|s| s.parse().expect(e!())).collect();
+    println!("{}", numbers.iter().sum::<i64>());
+    numbers.iter().sum()
 }
 
+fn day1p2() -> i64 {
+    let numbers: Vec<i64> = strings_input(1).into_iter().map(|s| s.parse().expect(e!())).collect();
+    let mut seen = HashSet::<i64>::new();
+    let mut s = 0;
+    loop {
+        for n in numbers.iter() {
+            seen.insert(s);
+            //println!("{} + {} = {}", s, n, s + n);
+            s += n;
+            if seen.contains(&s) {
+                println!("already seen: {}", s);
+                return s;
+            }
+        }
+    }
+}
+
+
+// for i in &mut v {
 
 
 // TODO: setup rust-fmt with vscode
